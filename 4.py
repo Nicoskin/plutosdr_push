3b5008629c4b69e65873da95bfb5a42beb534bf4
Nicoskin <56347254+Nicoskin@users.noreply.github.com>
2023-09-13 18:29:55 +0700

    Update 4.py

--- 4.py
+++ 4.py
@@ -1,9 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-v = 0.2
-f = 0
-A = 1
+v = 0.2 #частота
+f = 0 #фаза
+A = 1 #амплитуда
 
 x = np.arange(0,100,0.1)
 y = A*np.sin((x+f)*v)
@@ -11,7 +11,9 @@ z = A*np.cos((x+f)*v)
 
 plt.plot(x,y)
 plt.plot(x,z,'r')
-plt.title("A * sin(w*f*t)")
+
+plt.title("A * sin(w+f*t)")
 plt.xlabel("время")
 plt.ylabel("амплитуда")
+
 plt.show()
