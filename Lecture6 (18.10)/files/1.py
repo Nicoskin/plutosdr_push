bd80c899fa7ffa820158cbde81d70b0caa53326a
Пушница Владимир Алексеевич <ia131s20@csc.sibsutis.ru>
2023-10-18 22:59:09 +0700

    Сделал файл супер крутым

--- Lecture6 (18.10)/files/1.py
+++ Lecture6 (18.10)/files/1.py
@@ -3,85 +3,131 @@ import matplotlib.pyplot as plt
 import adi
 import time
 
-sdr = adi.Pluto("ip:192.168.2.1")
 
-frequency = 2300e6+(2e6*2)
-sdr.rx_lo = int(frequency)
-sdr.tx_lo = int(frequency)
+def sdr_settings(ip:str, frequency: int, buffer_size: int, sample_rate: int): # Настройка sdr
+    sdr = adi.Pluto(ip)
 
-sdr.rx_buffer_size = 1000
-sdr.sample_rate = 1e6
+    #frequency = 2300e6+(2e6*2)
+    sdr.rx_lo = int(frequency)
+    sdr.tx_lo = int(frequency)
 
+    sdr.rx_buffer_size = buffer_size
+    sdr.sample_rate = sample_rate
 
-fio = 'pushnitsa'
-encoded_bytes = fio.encode('ascii')
-# Преобразование байтов в массив битов
-bit_array = []
-for byte in encoded_bytes:
-    bits = bin(byte)[2:].zfill(8)  # Преобразование в биты
-    bit_array.extend([int(bit) for bit in bits])
-bit_start = np.ones(20)
-bit_stop = np.ones(10)
+    return sdr
 
-print(len(bit_array))
+def create_bit_str(fio: str): # Функция преобразования строки в битовую последовательность | Возвращает bit_array
+    #fio = 'pushnitsa'
+    encoded_bytes = fio.encode('ascii')
+    # Преобразование байтов в массив битов
+    bit_array = []
+    for byte in encoded_bytes:
+        bits = bin(byte)[2:].zfill(8)  # Преобразование в биты
+        bit_array.extend([int(bit) for bit in bits])
 
+    bit_start = np.ones(20) # 20 бит, тк в начале не всё передаётся
+    bit_stop = np.ones(10)
+    
+    # debug
+    #print('len(bit_array) =',len(bit_array)))
 
-bit_array_list = list(bit_array)
-bit_array_list = list(bit_start) + bit_array_list + list(bit_stop)
-bit_array = np.array(bit_array_list)
-print(len(bit_array))
+    # for i in range(len(bit_array)):
+    #     print(int(bit_array[i]), end=' ')
+    #     if (i + 1) % 8 == 0:
+    #         print()
 
-#sample = np.zeros(1024)
-sample = np.zeros(len(bit_array)*200, dtype=complex)
+    bit_array_list = list(bit_array)
+    bit_array_list = list(bit_start) + bit_array_list + list(bit_stop) # Добавление стартовых 16 бит и конечных 10
+    bit_array = np.array(bit_array_list)
 
-for i in range(len(bit_array)):
-    for o in range(100):
-        if bit_array[i] == 1:
-            sample[i * 100 + o] = 1 * 2 ** 14 + 1j * 2 ** 14
-        elif bit_array[i] == 0:
-            sample[i * 100 + o] = 1 * 2 ** 1 + 1j * 2 ** 1
+    # debug
+    #print('len(start+bit_array+stop) =', len(bit_array))
 
-rx = []
+    # for i in range(len(bit_array)):
+    #     print(int(bit_array[i]), end=' ')
+    #     if (i + 1) % 8 == 0:
+    #         print()
 
-#sdr.tx(sample)
-#sdr.tx_cyclic_buffer = True
-for i in range(1000):
-    if (i==200):
-       sdr.tx(sample) 
-    new_data = sdr.rx()
-    rx.extend(abs(new_data))
+    return bit_array
 
-threshold = 1500
-output = []
-start = 0
-consecutive_count = 0
-for i in range(len(rx)):
-    if rx[i] > threshold:
-        consecutive_count += 1
+def samples_from_bits(bit_array, symbol_length: int, amplitude_1: int, amplitude_0: int): # Функция преобразования samples в прямоугольный сигнал в зависимости от bit_array
+    sample = np.zeros(len(bit_array)*(symbol_length+10), dtype=complex) # sample массив из длинны бит*110 комплексных нулей (не 100 чтобы потом немного пустого места было)
+    for i in range(len(bit_array)):
+        for o in range(symbol_length):
+            if bit_array[i] == 1:
+                sample[i * symbol_length + o] = 1*amplitude_1 + 1j*amplitude_1
+            elif bit_array[i] == 0:
+                sample[i * symbol_length + o] = 1*amplitude_0 + 1j*amplitude_0
+    
+    return sample
 
-        if (consecutive_count == 1600):
-            start = 1
-            print('Start=',start, '  i =',i)
+def decoding_tx(rx, threshold: int, start_duration: int, stop_duratio: int): # Функция декодирует сигнал rx находя 1 и 0
+    output = []
+    start = 0
+    consecutive_count = 0
+    for i in range(len(rx)):
+        if rx[i] > threshold:
+            consecutive_count += 1
 
-        if ((consecutive_count == 99)&(start==1)):
-            output.append(1)
+            if (consecutive_count == 1600):
+                start = 1
+                print('Start=',start, '  i =',i) # debug
+
+            if ((consecutive_count == 99) and (start==1)):
+                output.append(1)
+                consecutive_count = 0
+
+            if ((consecutive_count == 900) and (start==1)):
+                start = 0
+                print('Start =',start, '  i =',i) # debug
+
+        else:
             consecutive_count = 0
 
-        if ((consecutive_count == 900)&(start==1)):
-            start = 0
-            print('Start =',start, '  i =',i)
+    return output
+
+def rx_sig(samples, tx_cycle: bool, start_tx: int): # Функция передает samples начиная с ±(start_tx * 1000 + 3000) семпла | Возвращает массив rx
+    sdr.tx_cyclic_buffer = tx_cycle
+    rx = []
+    for i in range(1000): # Считывает секунду Rx
+        if (i==start_tx): # На start_tx * 1000 сэмпле начать Tx
+            sdr.tx(samples)
+
+        new_data = sdr.rx()
+        rx.extend(abs(new_data))
+
+    return rx
+
+def out_main_graph(rx): # Функция выводит rx
+    plt.figure(1, figsize=[10,8])
+    plt.title("Сигнал")
+    plt.xlabel("samples") 
+    plt.ylabel("amplitude")
+    plt.xlim(202800, 203500+len(samples)) # вывод начиная с самого сигнала
+    plt.plot(rx)
 
 
-    else:
-        consecutive_count = 0
 
-print(" ")
-print(len(output))
-print(output)
+sdr = sdr_settings("ip:192.168.2.1", 2300e6+(2e6*2), 1000, 1e6)
 
-plt.figure(figsize=[10,8])
-plt.plot(rx)
-plt.xlim(202800,202000+len(bit_array)*150)
+bit_array = create_bit_str('raf')
+
+samples = samples_from_bits(bit_array, 100, 2**14, 2**1) 
+
+# plt.plot(samples) # debug
+# plt.show()
+
+rx = rx_sig(samples, False, 200)
+
+output = decoding_tx(rx, 1500, 1600, 900)
+
+# print(" ") # debug       
+# print(len(output))
+# print(output)       
+
+out_main_graph(rx) # Можно закоментить чтобы вывести что-то другое
+         
 # plt.figure(2)
 # plt.plot(sample)
+
 plt.show()
